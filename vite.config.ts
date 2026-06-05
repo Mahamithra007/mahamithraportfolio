496496a4d0fd689e2e6ec2e3065cd0807e4813c0
Mahamithra007 <mahamithra007@gmail.com>
2026-06-06 00:09:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     base: '/mahamithraportfolio/',
-    build: {
-      // Directs Vite to bundle the static single-page bundle into 'dist'
-      outDir: 'dist',
-    }
   },
   tanstackStart: {
     server: { 
